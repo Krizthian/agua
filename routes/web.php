ccf404655296a35c7fe879708ac239b6bbc0fa17
Cristian Ortiz <cristianortizmorales2013@gmail.com>
2023-07-02 10:00:27 -0500

    Routes: Web: Creación de rutas temporales
    Temporalmente se crearan rutas las cuales estaran comentadas e iremos utilizando o actualizando conforme se requieran. De momento utilizaremos la ruta Home

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home')->name('home');
+//Route::view('/about', 'about')->name('about');
+//Route::view('/contact', 'contact')->name('contact');
+//Route::view('/servicios', 'servicios')->name('servicios');
+//Route::view('/login', 'login')->name('login')
