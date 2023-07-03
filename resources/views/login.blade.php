7ae43da443a192aad5b92494006305eff5714e3b
Cristian Ortiz <cristianortizmorales2013@gmail.com>
2023-07-02 20:24:48 -0500

    Views: Login: Mejora de campos en formulario
    Para trabajar correctamente con el envio de formularios, los campos de estos deben tener identificadores apropiados, con los que se pueda trabajar en un modelo, así mismo, los campos deben tener restricciones que eviten que un formulario se envíe si este no tiene los campos requeridos.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,17 +6,17 @@
 <main class="form-signin w-100 m-auto">
   <form>
     <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Usuario</label>
+      <input type="text" class="form-control" id="usuario" name="usuario" placeholder="Usuario" required>
+      <label for="usuario">Usuario</label>
     </div>
     <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Contraseña</label>
+      <input type="password" class="form-control" id="password" name="password" placeholder="Contraseña" required>
+      <label for="password">Contraseña</label>
     </div>
     <button class="btn btn-success w-100 py-2" type="submit">Ingresar</button>
     <br>
     <br>
-    <center><a class="mt-5 mb-3 text-body-secondary">Regresar al inicio</a></center>
+    <center><a href="/" class="mt-5 mb-3 text-body-secondary">Regresar al inicio</a></center>
   </form>
 </main>
 </div>
