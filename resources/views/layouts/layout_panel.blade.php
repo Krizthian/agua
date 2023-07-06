21b1e25b614a40e53802ef234aea678f6f94df40
Cristian Ortiz <cristianortizmorales2013@gmail.com>
2023-07-06 13:56:01 -0500

    Views: layout_panel: Preparar cambio de botones de navbar a enlaces
    Hay demasiados botones en distintas partes de la pagina, lo mas apropiado en este tipo de menús es utilizar enlaces para las opciones, se ha utilizado botones, sin embargo empieza a ser molesto y distractorio.
    
    De momento no se realizará el cambio, sin embargo, se prepara todo para hacerlo en su debido momento, aún se debe evaluar esta decisión de diseño antes de realizar el cambio.

--- resources/views/layouts/layout_panel.blade.php
+++ resources/views/layouts/layout_panel.blade.php
@@ -20,14 +20,27 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-        <!--BOTONES MENU INICIO-->
-                  <a href="/panel"  type="button" class="btn btn-primary me-2">Consulta</a>
-                  <a href="/medidores" type="button" class="btn btn-success me-2">Medidores</a>
-                  <!--ESTE VINCULO SOLO SERA VISTO Y SERA ACCESIBLE PARA USUARIOS CON ROL DE ADMINISTRADOR-->
-                  <a href="/usuarios" type="button" class="btn btn-warning me-2">Usuarios</a> 
-                  <!--FIN VINCULO RESERVADO-->
-                  <a href="/reportes" type="button" class="btn btn-danger me-2">Reportes</a>
-                         
+        <!--    <li class="nav-item">
+                <a class="nav-link " aria-current="page" href="#">Consulta</a>
+              </li>
+            <li class="nav-item">
+                <a class="nav-link " aria-current="page" href="#">Medidores</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " aria-current="page" href="#">Usuarios</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link " aria-current="page" href="#">Reportes</a>
+              </li>-->
+            <!--BOTONES MENU INICIO-->
+              <a href="/panel"  type="button" class="btn btn-primary me-2">Consulta</a>
+              <a href="/medidores" type="button" class="btn btn-success me-2">Medidores</a>
+              
+              <!--ESTE VINCULO SOLO SERA VISTO Y SERA ACCESIBLE PARA USUARIOS CON ROL DE ADMINISTRADOR-->
+              <a href="/usuarios" type="button" class="btn btn-warning me-2">Usuarios</a> 
+              <!--FIN VINCULO RESERVADO-->
+
+              <a href="/reportes" type="button" class="btn btn-danger me-2">Reportes</a>   
          <!--BOTONES MENU FIN-->
         </li>
       </ul>
