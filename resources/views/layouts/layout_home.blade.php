94da66e6939122dcb7b23707d746210a9ab70015
Cristian Ortiz <cristianortizmorales2013@gmail.com>
2023-07-07 21:24:45 -0500

    Views: Layouts: Añadir favicon para todas las vistas
    Para seguir la linea establecida por la pagina del Municipio de Portovelo (https://www.portovelo.gob.ec/) se añade un icono que se mostrará en los distintos módulos de la aplicación en general.

--- resources/views/layouts/layout_home.blade.php
+++ resources/views/layouts/layout_home.blade.php
@@ -6,6 +6,7 @@
     <title>Inicio</title>
     <!--BOOTSTRAP 5.3 - CSS-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel='shortcut icon' href='{{asset('img/favicon.ico')}}' type='image/x-icon'>
     <!--ARCHIVO DE CSS PERSONALIZADO-->
     <link rel="stylesheet" href="{{url('css/custom.css')}}">
   </head>
--- resources/views/layouts/layout_panel.blade.php
+++ resources/views/layouts/layout_panel.blade.php
@@ -6,6 +6,7 @@
     <title>Panel de Gestión</title>
     <!--BOOTSTRAP 5.3-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel='shortcut icon' href='{{asset('img/favicon.ico')}}' type='image/x-icon'>
   </head>
   <body class="bg-body-tertiary">
     <header>
