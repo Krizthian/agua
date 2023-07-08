9f222ad80af55008e1c4713b1b731db76d3a1fab
Cristian Ortiz <cristianortizmorales2013@gmail.com>
2023-07-07 21:02:21 -0500

    Views: layout_panel: Reemplazar botones por enlaces
    No es correcto utilizar tantos botones, así mismo la cantidad de colores resulta distractoria y confusa para el usuario.
    
    Para hacerlo mas tradicional y cercano a una navbar, vamos a utilizar enlaces sin colores que cumpliran la misma funcion que los botones con la excepción de que esto no comete una falta enorme en cuanto a usabilidad y diseño de UI.

--- resources/views/layouts/layout_panel.blade.php
+++ resources/views/layouts/layout_panel.blade.php
@@ -20,28 +20,20 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-        <!--    <li class="nav-item">
-                <a class="nav-link " aria-current="page" href="#">Consulta</a>
+       <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="/panel">Consulta</a>
               </li>
             <li class="nav-item">
-                <a class="nav-link " aria-current="page" href="#">Medidores</a>
+                <a class="nav-link" aria-current="page" href="/medidores">Medidores</a>
               </li>
+            <!--ESTE VINCULO SOLO SERA VISTO Y SERA ACCESIBLE PARA USUARIOS CON ROL DE ADMINISTRADOR-->
               <li class="nav-item">
-                <a class="nav-link " aria-current="page" href="#">Usuarios</a>
+                <a class="nav-link" aria-current="page" href="/usuarios">Usuarios</a>
               </li>
+             <!--FIN VINCULO RESERVADO-->
               <li class="nav-item">
-                <a class="nav-link " aria-current="page" href="#">Reportes</a>
-              </li>-->
-            <!--BOTONES MENU INICIO-->
-              <a href="/panel"  type="button" class="btn btn-primary me-2">Consulta</a>
-              <a href="/medidores" type="button" class="btn btn-success me-2">Medidores</a>
-              
-              <!--ESTE VINCULO SOLO SERA VISTO Y SERA ACCESIBLE PARA USUARIOS CON ROL DE ADMINISTRADOR-->
-              <a href="/usuarios" type="button" class="btn btn-warning me-2">Usuarios</a> 
-              <!--FIN VINCULO RESERVADO-->
-
-              <a href="/reportes" type="button" class="btn btn-danger me-2">Reportes</a>   
-         <!--BOTONES MENU FIN-->
+                <a class="nav-link" aria-current="page" href="/reportes">Reportes</a>
+              </li>
         </li>
       </ul>
       <!--TEXTO DE SALUDO-->
