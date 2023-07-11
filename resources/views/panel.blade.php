37bbbcaccbc4c91e18337f84fe26d34bb2d8dbf4
Cristian Ortiz <cristianortizmorales2013@gmail.com>
2023-07-11 18:17:36 -0500

    Views: Panel: Establecer un botón de acuerdo al estado del servicio
    Hasta el momento solo se manejaba un botón para solicitar el corte del servicio, sin embargo, puede que en algún punto y con la aplicación ya operativa existan medidores cuyo servicio esta inactivo pero requiera ser reactivado.
    Por lo tanto, realizaremos una pequeña comprobación del estado del servicio dependiendo del medidor y en base a eso mostraremos una opción especifica.
    Si el medidor tiene el servicio ‘activo’ entonces se mostrará la opción para solicitar el corte.
    Por otro lado, si el medidor tiene el servicio ‘inactivo’ entonces se mostrará la opción para reactivar el servicio.

--- resources/views/panel.blade.php
+++ resources/views/panel.blade.php
@@ -69,13 +69,23 @@
                       <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718H4zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73l.348.086z"/>
                     </svg></a>
                     <!--FIN BOTON PAGAR-->
-                    <!--BOTON CORTAR SERVICIO-->
-                    <a type="button" href="cortar.php" class="btn btn-outline-danger" title="Suspender servicio"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                  </svg></a>
-                    <!--FIN BOTON CORTAR SERVICIO-->
-
+                  <!--INICIO DE COMPROBACION DE ESTADO DE SERVICIO-->  
+                    @if($valoresPagarItem->estado_servicio == "activo") 
+                     <!--BOTON CORTAR SERVICIO-->
+                              <a type="button" href="cortar.php" class="btn btn-outline-danger" title="Suspender servicio"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                              <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                            </svg></a>
+                      <!--FIN BOTON CORTAR SERVICIO-->
+                    @else 
+                      <!--BOTON RESTAURAR SERVICIO-->
+                              <a type="button" href="restore.php" class="btn btn-outline-info" title="Reactivar servicio"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
+                              <path fill-rule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z"/>
+                              <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z"/>
+                            </svg></a>
+                      <!--FIN BOTON RESTAURAR SERVICIO-->
+                    @endif
+                  <!--FIN DE COMPROBACION DE ESTADO DE SERVICIO-->  
               </td>
             </tr> 
             @endforeach
