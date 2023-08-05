bb79470565d9ba415851954eaa6722db60950cf5
Cristian Ortiz <cristianortizmorales2013@gmail.com>
2023-08-05 13:18:29 -0500

    Views: Panel: Añadir botones de retorno a algunos formularios
    Para corregir un problema de usabilidad, vamos a añadir un botón de "Regresar" en cada formulario de ingreso o edición, aún hay algunos formularios pendientes como son el de:
    -Formulario de creación de cliente
    -Formulario de creación de medidor
    -Formulario de edición de medidor
    
    Sin embargo, ya se añadirán esos botones mas adelante en futuros cambios.

--- resources/views/clientes/editar.blade.php
+++ resources/views/clientes/editar.blade.php
@@ -13,6 +13,11 @@
 <center><h1 class="display-4">EDITAR CLIENTE</h1></center>
     <div class="container">
       <br>
+        <!--BOTON DE REGRESAR-->
+          <div title="Regresar" class="col-md-12 bg-light text-right"><a href="{{route('clientes.index')}}" type="submit" class="btn btn-primary float-start"><svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-arrow-return-left" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"/>
+          </svg></a></div>
+      <!--FIN DE BOTON DE REGRESAR-->
         <main class="form-signin w-100 m-auto">
          <form action="{{route('clientes.update', $clientesItem)}}" method="POST">
          	@csrf @method('PATCH')
@@ -30,7 +35,6 @@
               @endif
             <!--FIN DE MENSAJES DE ERROR-->  
            <div class="col-auto">
-            <label>Numero de medidor:</label><center><input type="text" class="form-control" name="numero_medidor" value="{{$clientesItem->numero_medidor}}" placeholder="{{$clientesItem->numero_medidor}}" disabled></input></center>
             <label>Nombre del cliente:</label><center><input type="text" class="form-control" name="nombre" value="{{$clientesItem->nombre}}" placeholder="{{$clientesItem->nombre}}" required></input></center>
             <label>Apellido del cliente:</label><center><input type="text" class="form-control" name="apellido" value="{{$clientesItem->apellido}}" placeholder="{{$clientesItem->apellido}}" required></input></center>
             <label>Cédula del cliente:</label><center><input type="text" class="form-control @error('cedula') is-invalid @enderror" name="cedula" value="{{old('cedula',$clientesItem->cedula)}}" placeholder="{{$clientesItem->cedula}}" required></input></center>
