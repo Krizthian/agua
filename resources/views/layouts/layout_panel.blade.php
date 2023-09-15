87c2157c30a29b8e24cf4fbbcabcdf080b1ece41
Cristian Ortiz <cristianortizmorales2013@gmail.com>
2023-09-15 17:03:58 -0500

    Views: Optimizar carga de recursos CSS y JS
    Es buena practica utilizar CDNs para importar los archivos CSS y JS, sin embargo, hay algunos que es preferible tenerlos en el servidor.
    
    Por esto vamos a mantener los archivos de jQuery y Bootstrap (CSS) en el servidor, solo para evitar obtener tantos archivos de distintos CDNs.
    
    Adicionalmente, vamos a corregir un error y es que anteriormente la carga de alertas (SweetAlerts) fallaba debido al cambio realizado en https://github.com/Krizthian/agua/commit/74e5b85e0fc8652a128d207ba90b3afab10322c0 Esto debido a que se le asigno el atributo defer, que hace que no se priorice la carga de este script, lo mismo con jQuery, vamos a quitarles ese atributo a ambos puesto que es importante obtenerlos en primera instancia para asegurar que todas las caracteristicas de la aplicación funcionen correctamente.
    
    Se seguira realizando pruebas para asegurar que la carga de scripts y CSS sea optima y asi evitar problemas de rendimiento o que algunas caracteristicas no funcionen.

--- resources/views/layouts/layout_panel.blade.php
+++ resources/views/layouts/layout_panel.blade.php
@@ -13,12 +13,12 @@
       <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.1.1/css/buttons.bootstrap4.min.css">
       
       <!--BOOTSTRAP 5.3-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link href="{{url('css/bootstrap.min.css')}}" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/all.css">
         <link rel='shortcut icon' href='{{asset('img/favicon.ico')}}' type='image/x-icon'>
 
       <!--DATATABLE-->   
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js" defer></script>
+        <script src="{{asset('js/jquery.min.js')}}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js" defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js" defer></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js" defer></script>
@@ -27,11 +27,10 @@
         <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js" defer></script>
         <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.bootstrap5.min.js" defer></script>
         <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js" defer></script>
-        <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js" defer></script>
+        <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js" defer></script>    
       <!--FIN DATATABLE-->
     <!--INICIO DE SWEETALERTS-->
-           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.20/dist/sweetalert2.min.css">
-          <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.20/dist/sweetalert2.all.min.js" defer></script>     
+           <link rel="stylesheet" href="{{url('css/sweetalert2.min.css')}}">
     <!--FIN DE SWEETALERTS-->
     <link rel="stylesheet" href="{{url('css/custom.css')}}">
     <link rel="stylesheet" href="{{url('css/footer.css')}}">
@@ -146,6 +145,7 @@
 </div>
 <!--FIN FOOTER-->
   <!--JAVASCRIPT BOOTSTRAP 5.3-->
+      <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.20/dist/sweetalert2.all.min.js"></script> 
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous" defer></script>
 </html>
 
